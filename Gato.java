a272009e5ee73a13891514d68352e632adfdb9f0
joburbanop <95501946+joburbanop@users.noreply.github.com>
2023-02-10 08:52:40 -0500

    creando clases y atributos

---
+++ Gato.java
@@ -0,0 +1,19 @@
+public class Gato {
+    /*----------------------
+     * ATRIBUTOS
+     *-----------*/
+    private String nombre;
+    private int edad;
+    private String dueno;
+
+
+    /*------------------------
+     * METODOS
+     *-----------------*/
+    public  Gato(String nombre, int edad, String dueno) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dueno = dueno;
+    }
+
+}
