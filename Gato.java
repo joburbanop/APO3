c1220caaa3992909e8f5a575ee8c735311c54a17
joburbanop <joburbanop589@gmail.com>
2023-02-10 13:00:24 -0500

    arreglando los metodos

--- Gato.java
+++ Gato.java
@@ -14,6 +14,31 @@ public class Gato {
         this.nombre = nombre;
         this.edad = edad;
         this.dueno = dueno;
+        dormir();
+        comer();
+        jugar();
+
     }
+
+    public void dormir(){
+        System.out.println("Estoy durmiendo");
+
+    }
+
+    public void comer(){
+        System.out.println("Estoy comiendo");
+
+    }
+    
+    public void jugar(){
+        System.out.println("Estoy jugando con: " + this.nombre);
+
+    }
+    public void correr(){
+        System.out.println("Estoy corriendo");
+
+    }
+
+
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-import Gato.java;
+
 
 public class Main {
     public static void main(String[] args) {
-            
+        Gato llamar = new Gato("paco", 5, "NN");
+
+        llamar.jugar();
+        llamar.comer();
+
     }
 }
